6461df1f7f017c5047fb43d219625c1e73948a5a
Camilo Torres <camilo@ulises.udistritaloas.edu.co>
2017-11-27 10:34:31 -0500

    Se agrega el relatedsel de informacion_persona_natural

--- models/informacion_persona_natural.go
+++ models/informacion_persona_natural.go
@@ -86,7 +86,7 @@ func GetInformacionPersonaNaturalById(id string) (v *InformacionPersonaNatural, 
 func GetAllInformacionPersonaNatural(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(InformacionPersonaNatural))
+	qs := o.QueryTable(new(InformacionPersonaNatural)).RelatedSel(5)
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
